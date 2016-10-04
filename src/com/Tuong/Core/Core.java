61baf3525939aa444a3de05726807535c2656c3f
Walter Frost <walterfrost.dev@gmail.com>
2016-10-04 10:17:05 +0700

    Add plugin.yml

--- src/com/Tuong/Core/Core.java
+++ src/com/Tuong/Core/Core.java
@@ -4,6 +4,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin implements Listener{
+	//Plugin created by Nguyen Lam Tuong
 	public void onEnable(){
 		
 	}
