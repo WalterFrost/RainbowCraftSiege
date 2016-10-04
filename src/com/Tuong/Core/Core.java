61fb01a65d9e8562410c74b84fe9586265c3b782
Walter Frost <walterfrost.dev@gmail.com>
2016-10-04 10:11:49 +0700

    First Commit

---
+++ src/com/Tuong/Core/Core.java
@@ -0,0 +1,10 @@
+package com.Tuong.Core;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin implements Listener{
+	public void onEnable(){
+		
+	}
+}
